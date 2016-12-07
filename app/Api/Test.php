59cdd8eb7faae5a82e1c86908207c9e4ea3cdfce
Tim <l1111j@live.com>
2016-12-07 15:08:30 +1100

    updated a test class

---
+++ app/Api/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\Api;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class Test extends Controller
+{
+	public function output()
+	{
+		echo "hello";
+	}
+}
---
+++ app/Http/Controllers/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class Test extends Controller
+{
+	public function output()
+	{
+		echo "hello";
+	}
+}
